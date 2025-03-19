9bf5f935dbb4baf0b915af55019f3e6b06977fb9
noirandmoca <noirandmoca@gmail.com>
2025-03-19 21:50:48 +0900

    3/19

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
 //スティッキーヘッダー
-$(function(){
+$(function () {
   var $win = $(window),
-      $fv = $('.fv'),
-      $header = $('.header'),
-      fvHeight = $fv.outerHeight();
-      fixedClass = 'fixed';
-     
-      $win.on('load scroll',function(){
-        var value = $(this).scrollTop();
-        if($win.width() > 768){
-          if( value > fvHeight){
-            $header.addClass(fixedClass);
-          }else{
-            $header.removeClass(fixedClass);
-          }
-        }
-      });
+    $fv = $('.fv'),
+    $header = $('.header'),
+    fvHeight = $fv.outerHeight();
+  fixedClass = 'fixed';
+
+  $win.on('load scroll', function () {
+    var value = $(this).scrollTop();
+    if ($win.width() > 768) {
+      if (value > fvHeight) {
+        $header.addClass(fixedClass);
+      } else {
+        $header.removeClass(fixedClass);
+      }
+    }
+  });
 });
 
 //スライダー
-$('.slider').slick({
-autoplay: true,
-autoplaySpeed: 2000,
-fade: true,
-speed: 1000,
-cssEase: 'linear'
-});
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    speed: 1000,
+    cssEase: 'linear'
+  });
 
 //ハンバーガーメニュー
-$('.burger-btn').on('click',function(){
+$('.burger-btn').on('click',function (){
   $('.header-nav').fadeToggle(300);
   $(this).toggleClass('cross');
-$('body').toggleClass('noscroll');
+  $('body').toggleClass('noscroll');
 });
